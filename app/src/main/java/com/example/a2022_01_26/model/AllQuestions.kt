0fe4ba41d42796fc5b84e2f9c267d3fbd68f6c96
Hackeem Bassaragh <hackeem@Hackeems-Air.home>
2022-02-05 20:50:09 -0500

    first commit

---
+++ app/src/main/java/com/example/a2022_01_26/model/AllQuestions.kt
@@ -0,0 +1,20 @@
+package com.example.a2022_01_26.model
+
+import com.example.a2022_01_26.R
+
+class AllQuestions {
+    public val allQuestions = arrayOf(
+        Question(R.string.q1, true),
+        Question(R.string.q2, false),
+        Question(R.string.q3, false),
+        Question(R.string.q4, true),
+        Question(R.string.q5, false),
+        Question(R.string.q6, true),
+        Question(R.string.q7, true),
+        Question(R.string.q8, false),
+        Question(R.string.q9, false),
+        Question(R.string.q10, false),
+    )
+
+
+}
